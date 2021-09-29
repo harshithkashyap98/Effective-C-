35f12598f1c19f194754ed3d8c3f902c242a450c
harshithkashyap98 <91448047+harshithkashyap98@users.noreply.github.com>
2021-09-29 09:34:05 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace multicastdemo
+{
+    class Program
+    {
+        static void Test() { }
+
+        static void NewTest() { }
+
+
+        static void Main(string[] args)
+        {
+            Action funObj1 = new Action(Program.Test);
+            Action funObj2 = new Action(Program.NewTest);
+
+            //funObj1 = (Action) System.Delegate.Combine(funObj1, funObj2);
+
+            funObj1 = funObj1 + funObj2;
+            Broadcast(funObj1);
+
+            //can also do: funObj1 = funObj1 - funObj2
+
+        }
+
+        static void Broadcast (Action funcObj)
+        {
+            funcObj.Invoke();
+        }
+    }
+}
